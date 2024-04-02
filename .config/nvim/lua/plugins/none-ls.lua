cbd9848a202c4c313ca4cd61df90230c0cd08e48
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2024-04-02 10:18:23 +0200

    Using LazyVim as default neovim editor

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,7 +1,7 @@
 return {
   "nvimtools/none-ls.nvim",
 
-  config = function()
+  opts = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
@@ -12,7 +12,7 @@ return {
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports
+        null_ls.builtins.formatting.goimports,
       },
     })
 
