24c10a1726fc707f52552c39105ba6f513696354
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2024-05-21 22:34:58 +0200

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -4,30 +4,30 @@
 ## It will setup all the necessary tools for development and also setup dotfiles
 ## This can be run before cloning the dotfiles repository
 
-# Update
+# Updating and setting up system tools and dependencies
 sudo apt update -y
 sudo apt install -y \
 	git curl docker.io build-essential pkg-config stow \
-	tmux tmuxinator snapd ripgrep \
+	tmux tmuxinator snapd \
 	libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
 	libvips imagemagick libmagickwand-dev \
 	libssl-dev clang \
 	imagemagick libmagickwand-dev
 
 # Clone dotfiles
+echo "Cloning dotfiles"
 git clone https://github.com/jonashan/dotfiles.git ~/dotfiles
 
 # Install TPM for Tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Stowing the dotfiles (creates symlink)
+echo "Stowing dotfiles"
 cd ~/dotfiles
 stow .
 cd ~/
 
-# Install Starship
-sudo snap install starship --edge
-
+echo "Installing Ruby and Nodejs"
 # asdf - Ruby and Node.js
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 echo '. "$HOME/.asdf/asdf.sh"' >>~/.bashrc
@@ -44,19 +44,19 @@ EOF
 
 asdf install
 
-# Setting up bashrc
+# Setting up aliases
+echo "Setting up aliases"
 cat <<EOF >>~/.bashrc
 # Aliases
 source ~/.aliases
 EOF
-echo 'eval "$(starship init bash)"' >>~/.bashrc
 source ~/.bashrc
 
 # Setup Neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.bashrc
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update -y
+sudo apt install -y ripgrep neovim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
 source ~/.bashrc
 
 # Setup LazyGit
@@ -66,6 +66,16 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 # Starting docker containers
+echo "Setup docker user and start the containers"
+sudo usermod -aG docker ${USER}
+# Needs to sudo at the first run because usermod is not effective before reboot
 sudo docker run -d --restart unless-stopped -p 5432:5432 --name=postgres -e POSTGRES_PASSWORD=postgres postgres
 sudo docker run -d --restart unless-stopped -p 6379:6379 --name=redis redis
 # docker run -d --restart unless-stopped -p 3306:3306 --name=mysql5.7 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:5.7
+
+
+# Install Starship
+sudo snap install starship --edge
+echo "Launching starship"
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+source ~/.bashrc
