abed3469588e25ac28831aea059c34e2b9145b6a
Jonas Hansen <jonas@jonas-hansen.dk>
2025-04-06 21:05:05 +0200

    Updated lazyvim and removed copilot

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.copilot_filetypes = { markdown = true }
+-- vim.g.copilot_filetypes = { markdown = true }
 
 -- Disable swap files
 vim.opt.swapfile = false
